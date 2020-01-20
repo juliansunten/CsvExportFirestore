090d3acd3b8564c637cc238f3763f46cd0d3d7af
juliansunten <julian.sunten@every-websolution.com>
2020-01-20 18:15:56 +0100

    removed Cred

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDe3wlYqn4k6YfNWy7Tb_-Q13H535QlsqY',
-    authDomain: 'albrechtkom-retour.firebaseapp.com',
-    databaseURL: 'https://albrechtkom-retour.firebaseio.com',
-    projectId: 'albrechtkom-retour',
-    storageBucket: 'albrechtkom-retour.appspot.com',
-    messagingSenderId: '121482845597',
-    appId: '1:121482845597:web:dfad709f88412fea4ee374'
+    '/FIREBASE CONFIG'
   }
 };
